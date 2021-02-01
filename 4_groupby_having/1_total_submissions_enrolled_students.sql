184755d87183a395a228291ea5b2a6effaeee58c
Rebecca Chen <rebecca061996@gmail.com>
2021-02-01 19:34:38 +0000

    create queries to get total submissions for currently enrolled students, and filtering for those with less than 100 submissions

---
+++ 4_groupby_having/1_total_submissions_enrolled_students.sql
@@ -0,0 +1,7 @@
+-- get total submissions for students and total submissions
+-- enrolled students only
+SELECT students.name AS student, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
---
+++ 4_groupby_having/2_total_submissions_under_100.sql
@@ -0,0 +1,8 @@
+-- get total submissions for currently enrolled students
+-- total submissions less than 100
+SELECT students.name AS student, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING COUNT(assignment_submissions.*) < 100;
