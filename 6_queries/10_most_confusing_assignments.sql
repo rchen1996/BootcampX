057740ba6f0838440f2c0f031397b928d16d1496
Rebecca Chen <rebecca061996@gmail.com>
2021-02-02 00:52:48 +0000

    create query to list each assignment with the total number of ARs with it

---
+++ 6_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,8 @@
+-- list each assignment with total ARs
+-- assignment's id, day, chapter, name, total ARs
+-- order by total ARs from most to least
+SELECT assignments.id, name, day, chapter, COUNT(assistance_requests) AS total_requests
+FROM assignments
+JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
