878c6345cecbb6e16ecd0f10c5ee1be558af2e1b
Rebecca Chen <rebecca061996@gmail.com>
2021-02-03 18:15:34 +0000

    update to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [`%${process.argv[2]}%`, process.argv[3]];
+
 pool.query(`
   SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
   FROM students
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-`)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [process.argv[2]];
+
 pool.query(`
   SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
   FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
+  WHERE cohorts.name = $1
   ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(ar => {
       console.log(`${ar.cohort}: ${ar.teacher}`);
